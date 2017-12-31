5403097d454245804d8d38501593a74dfb09cc53
jsenjobs <jsen1922279340@163.com>
2017-12-31 13:52:43 +0800

    C

--- app/model/index.js
+++ app/model/index.js
@@ -1,3 +1,4 @@
 exports.userClient = require('./mongo/user.schema.js').userClient
 exports.userServer = require('./mongo/user.schema.js').userServer
 exports.deliver = require('./mongo/water.order.schema.js').deliver
+exports.porder = require('./mongo/water.order.schema.js').porder
